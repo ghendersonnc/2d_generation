493099d19f564cc7ded429c5cb36ee96b5b3c97a
ghendersonnc <ghendersonnc@gmail.com>
2026-02-15 00:49:59 -0500

    Queue system for chunking

--- 2d_generation/include/engine/world.h
+++ 2d_generation/include/engine/world.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <tuple>
+#include <vector>
 #include <unordered_map>
-
+#include <queue>
+#include <utility>
 
 #include <fw_graphics/shader.h>
 
@@ -20,7 +22,14 @@ namespace Fw::Engine
         void render(std::unordered_map<Config::Shader::Name, Graphics::Shader>& shaders);
     private:
         std::vector<Meshes::ChunkMesh> _meshes;
-        std::unordered_map<std::tuple<int, int>, Engine::Chunk> _chunks;
+        std::queue<std::pair<int, int>> _chunkQueue;
+        std::unordered_map<std::tuple<int, int>, Chunk> _chunks;
+        int _lastChunkPositionX;
+        int _lastChunkPositionY;
         int _chunkCount = 0;
+        bool _loaded;
+
+        void generateNewChunks();
+        void determineChunksToRender();
     };
 }
